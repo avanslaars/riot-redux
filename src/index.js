53a34b66cee8c6e6fa4f1a241b52aca41e1988b7
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-12 16:16:42 -0500

    Added return state to reducer in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var redux = require('redux')
 require('./tags/sample-output.tag')
 
 var reducer = function(state,action){
-  
+  return state
 }
 
 document.addEventListener('DOMContentLoaded', () => riot.mount('sample-output'))
