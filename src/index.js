5a455e5ab70d394eee76bcff5d65872cca8fe522
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-12 16:16:22 -0500

    Added empty reducer function to index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ var riot = require('riot')
 var redux = require('redux')
 require('./tags/sample-output.tag')
 
+var reducer = function(state,action){
+  
+}
+
 document.addEventListener('DOMContentLoaded', () => riot.mount('sample-output'))
