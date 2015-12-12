02feb2990b7eec79d60800e550ed1c50edb4f91c
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-12 17:57:22 -0500

    Added require for new tag to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var riot = require('riot')
 var redux = require('redux')
 require('./tags/sample-output.tag')
+require('./tags/title-form.tag')
 
 var reducer = function(state={title:'Default title'},action){
   switch(action.type){
