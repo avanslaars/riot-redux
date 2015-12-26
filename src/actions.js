3185f475b1305d1e56333979f39dc623685afedd
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-26 12:51:25 -0500

    Added request.send

--- src/actions.js
+++ src/actions.js
@@ -7,7 +7,8 @@ function loadTasks(){
     var request = new XMLHttpRequest();
     request.open('GET', 'http://localhost:3000/tasks', true);
     request.onload = function() {
-      
+
     }
+    request.send()
   }
 }
