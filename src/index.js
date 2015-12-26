ec85ba230f66614c75e9dee6597aeff4759525ac
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-26 11:44:30 -0500

    index.js - added thunk = require

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 var riot = require('riot')
 var redux = require('redux')
-
+var thunk = require('redux-thunk')
 
 var reducer = function(state={title:'Default title'},action){
   console.log(action)
   switch(action.type){
-    
+
     default:
       return state
   }
