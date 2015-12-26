245cbaa4362124c19708840370dfd3751b9f65c7
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-26 12:52:07 -0500

    Added if for success in onload handler

--- src/actions.js
+++ src/actions.js
@@ -7,7 +7,9 @@ function loadTasks(){
     var request = new XMLHttpRequest();
     request.open('GET', 'http://localhost:3000/tasks', true);
     request.onload = function() {
-
+      if (request.status >= 200 && request.status < 400) {
+        
+      }
     }
     request.send()
   }
