11c9e5eaa5cbf055bbbc4ea1775a80fe603ec3e4
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-26 14:43:12 -0500

    Wrapped data code in if to verify success

--- src/actions.js
+++ src/actions.js
@@ -37,8 +37,10 @@ function addTask(newTask){
     request.open('POST', 'http://localhost:3000/tasks', true)
     request.setRequestHeader("Content-Type","application/json")
     request.onload = function(){
-      var data = JSON.parse(request.responseText)
-      dispatch(newTaskAdded(data.id, data.name))
+      if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(request.responseText)
+        dispatch(newTaskAdded(data.id, data.name))
+      }
       dispatch(toggleLoading(false))
     }
 
