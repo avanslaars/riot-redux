39c2c272f50667f4ee1226cc8904d8b690194a79
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-26 13:00:46 -0500

    Updated default state in reducer - index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var riot = require('riot')
 var redux = require('redux')
 var thunk = require('redux-thunk')
 
-var reducer = function(state={title:'Default title'},action){
+var reducer = function(state={tasks:[]},action){
   console.log(action)
   switch(action.type){
 
