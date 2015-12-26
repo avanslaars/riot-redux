67ae99344f2a690388006a33db568025e76ea9aa
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-26 16:52:20 -0500

    Added empty concat with comment placeholder

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ var reducer = function(state={tasks:[]},action){
       //TODO 2: Update the isComplete flag on the target task
       //TODO 3: Create a new array with the updated task and the original tasks
       var newTasks = state.tasks.slice(0,taskIndex)
+        .concat(/*The updated task goes here*/)
       //TODO 4: Create a new state object that uses the updated tasks array
     default:
       return state
