5135bfe8b5dec71433bcb17c65a4b1d4f9fb24f1
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-26 12:50:55 -0500

    Added empty onload handler

--- src/actions.js
+++ src/actions.js
@@ -6,5 +6,8 @@ function loadTasks(){
   return function(dispatch,getState){
     var request = new XMLHttpRequest();
     request.open('GET', 'http://localhost:3000/tasks', true);
+    request.onload = function() {
+      
+    }
   }
 }
