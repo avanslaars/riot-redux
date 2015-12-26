cde5c31eeb7ff744570027e7b4b14340b865a6b4
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-12-26 12:48:19 -0500

    Added request.open with tasks API url

--- src/actions.js
+++ src/actions.js
@@ -5,5 +5,6 @@ module.exports = {
 function loadTasks(){
   return function(dispatch,getState){
     var request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:3000/tasks', true);
   }
 }
